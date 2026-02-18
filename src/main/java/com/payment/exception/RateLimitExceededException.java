6a2d67f1a01a174524e5f060e2d88a8635339ae4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-19 00:07:49 +0100

    Create RateLimitExceededException.java

---
+++ src/main/java/com/payment/exception/RateLimitExceededException.java
@@ -0,0 +1,15 @@
+package com.payment.exception;
+
+public class RateLimitExceededException extends RuntimeException {
+
+    private final int retryAfterSeconds;
+
+    public RateLimitExceededException(String message, int retryAfterSeconds) {
+        super(message);
+        this.retryAfterSeconds = retryAfterSeconds;
+    }
+
+    public int getRetryAfterSeconds() {
+        return retryAfterSeconds;
+    }
+}
