c44bccb3609fe126ea58874170a7c44d329dcac4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-23 00:09:26 +0100

    Create OpenApiConfig.java

---
+++ src/main/java/com/payment/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.payment.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Payment Processing API")
+                        .version("1.0")
+                        .description("Stripe-lite Payment Processing API for banks/fintech"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .addSecurityItem(new SecurityRequirement().addList("ApiKey"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT for dashboard/auth endpoints"))
+                        .addSecuritySchemes("ApiKey",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-API-KEY")
+                                        .description("API Key for payment endpoints (server-to-server)")));
+    }
+}
